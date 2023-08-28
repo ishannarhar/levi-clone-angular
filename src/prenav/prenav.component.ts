954f8eb9349b3521d07a02cccfc63c8c8184eb19
P3065049 <C-Ishan.Narhar@charter.com>
2023-08-28 16:14:46 +0530

    added grid, failing while adding images

--- src/prenav/prenav.component.ts
+++ src/prenav/prenav.component.ts
@@ -1,7 +1,11 @@
+declare var require: any;
+const imgname = require('./assets/indiaFlagIcon.png').default as string;
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-prenav',
   templateUrl: './prenav.component.html',
   styleUrls: ['./prenav.component.css'],
 })
-export class PrenavComponent {}
+export class PrenavComponent {
+  imgname = imgname;
+}
