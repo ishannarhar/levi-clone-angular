d4e8cbff443f51c4fedf6c81faac30b6d0b0770a
P3065049 <C-Ishan.Narhar@charter.com>
2023-08-28 18:27:21 +0530

    finishing the prenav component

--- src/prenav/prenav.component.ts
+++ src/prenav/prenav.component.ts
@@ -1,11 +1,7 @@
-declare var require: any;
-const imgname = require('./assets/indiaFlagIcon.png').default as string;
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-prenav',
   templateUrl: './prenav.component.html',
   styleUrls: ['./prenav.component.css'],
 })
-export class PrenavComponent {
-  imgname = imgname;
-}
+export class PrenavComponent {}
