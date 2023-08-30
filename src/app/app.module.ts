1ba6ee627413f29bc645719630b0604a15b263f7
P3065049 <C-Ishan.Narhar@charter.com>
2023-08-30 11:51:27 +0530

    designing the navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PrenavComponent } from '../prenav/prenav.component';
-
+import { NavbarComponent } from 'src/navbar/navbar.component';
 @NgModule({
-  declarations: [AppComponent, PrenavComponent],
+  declarations: [AppComponent, PrenavComponent, NavbarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
